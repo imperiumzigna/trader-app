8e1ee6d789c1875dcfde726d6582291e43d47232
Igor Amorim Silva <igoramorimsilva2@gmail.com>
2022-03-21 09:31:31 -0300

    added login requests test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   private
     def after_sign_in_path_for(resource)
+      sign_out resource unless current_user.active?
       home_path
     end
 end
---
+++ spec/requests/login_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Login', type: :request do
+  describe 'POST /login' do
+    let(:user) { FactoryBot.create(:user, active: false) }
+
+    context 'when user is not active' do
+      it 'redirects to login page' do
+        post '/login', params: { user: { email: user.email, password: 'Password1*' } }
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context 'when user is active' do
+      it 'redirects to home page' do
+        user.update(active: true)
+        post '/login', params: { user: { email: user.email, password: 'Password1*' } }
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+  end
+end
