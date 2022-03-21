e6de60e91686bac3099cf5212349a2323f22f602
Igor Amorim Silva <igoramorimsilva2@gmail.com>
2022-03-21 08:06:07 -0300

    added home page
    	new file:   app/assets/javascripts/home.js
    	new file:   app/assets/stylesheets/home.scss
    	modified:   app/controllers/application_controller.rb
    	new file:   app/controllers/home_controller.rb
    	modified:   app/controllers/users/registrations_controller.rb
    	new file:   app/helpers/home_helper.rb
    	modified:   app/models/user.rb
    	new file:   app/views/home/index.html.erb
    	new file:   app/views/shared/_nav_home.html.erb
    	new file:   app/views/shared/_side_home.html.erb
    	modified:   config/initializers/devise.rb
    	modified:   config/routes.rb
    	new file:   spec/helpers/home_helper_spec.rb
    	new file:   spec/requests/home_spec.rb
    	new file:   spec/views/home/index.html.erb_spec.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  private
+
+  def after_sign_in_path_for(resource)
+    home_index_path
+  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -53,11 +53,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     # The path used after sign up.
     def after_sign_up_path_for(resource)
-      super(resource)
+      home_index_path
     end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+   def after_inactive_sign_up_path_for(resource)
+     super(resource)
+   end
 end
